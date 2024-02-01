6a75124d7c4a289d2c8ab2177338e791cfca1dc2
induc <67059026+Induc85@users.noreply.github.com>
2024-02-01 14:41:09 +0530

    Create newspage.js
    Javascript to populate data from json

---
+++ newspage.js
@@ -0,0 +1,40 @@
+function loadArticles() {
+    var newsArticlesDiv = document.getElementById('newsArticles');
+    var articleContent = "";
+    var fetchArticles = fetch('http://localhost:3000/code-test.json', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+   .then(response => response.json())
+   .then(response => {
+    console.log(JSON.stringify(response))
+        for(var i=0; i<response.articles.length; i++){
+            articleContent += `<article class="listArticle">
+                           <h4>${response.articles[i].primarySectionRouteName}</h4>
+                               <div class="flex-container">
+                               <div class="news-headline">
+                               <h3>${response.articles[i].headline}</h3>
+                               <p>${response.articles[i].standfirst}</p>
+                               </div>
+                               <div class="news-img">
+                               <img src="${response.articles[i].thumbnail.src}"/>
+                               </div>
+                               </div>
+                               <hr>
+                               </article>`;
+        }    
+        newsArticlesDiv.innerHTML += articleContent;
+    }
+    
+    );
+
+
+
+    
+}
+
+loadArticles();
+
+    

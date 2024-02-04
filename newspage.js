e714959273bc9ca51c454a4ca8c57b003d81442f
induc <67059026+Induc85@users.noreply.github.com>
2024-02-04 23:47:00 +0530

    Update newspage.js
    changed json fetch with Async / Await promises

--- newspage.js
+++ newspage.js
@@ -1,35 +1,41 @@
 function loadArticles() {
     let newsArticlesDiv = document.getElementById('newsArticles');
     let articleContent = "";
-    let fetchArticles = fetch('http://localhost:3000/code-test.json', {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-        },
-    })
-   .then(response => response.json())
-   .then(response => {
-    console.log(JSON.stringify(response))
-        for(let i=0; i<response.articles.length; i++){
-            articleContent += `<article class="listArticle">
-                           <h4>${response.articles[i].primarySectionRouteName}</h4>
-                               <div class="flex-container">
-                               <div class="news-headline">
-                               <h3>${response.articles[i].headline}</h3>
-                               <p>${response.articles[i].standfirst}</p>
-                               </div>
-                               <div class="news-img">
-                               <img src="${response.articles[i].thumbnail.src}"/>
-                               </div>
-                               </div>
-                               <hr>
-                               </article>`;
-        }    
-        newsArticlesDiv.innerHTML += articleContent;
+   
+    async function fetchArticlesJson() {
+        const response = await fetch(
+            'http://localhost:3000/code-test.json',
+                {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                    }
+                }
+            );
+        const articles = await response.json();
+        return articles;
     }
-    
-    );
 
+    fetchArticlesJson().then(articles => {
+        const ArticlesObj = articles.articles; // fetched articles
+        for(let i=0; i<ArticlesObj.length; i++) {
+            articleContent += `<article class="listArticle">
+                            <h4>${ArticlesObj[i].primarySectionRouteName}</h4>
+                                <div class="flex-container">
+                                <div class="news-headline">
+                                <h3>${ArticlesObj[i].headline}</h3>
+                                <p>${ArticlesObj[i].standfirst}</p>
+                                </div>
+                                <div class="news-img">
+                                <img src="${ArticlesObj[i].thumbnail.src}"/>
+                                </div>
+                                </div>
+                                <hr>
+                                </article>`;
+        }   
+        
+        newsArticlesDiv.innerHTML += articleContent;
+    });
 
 
     
